b220d37bddc6291cd72f5ad5ddda9aa3d0548267
Francisco Perdigon Romero <fperdigon88@gmail.com>
2020-02-11 16:44:21 -0500

    Create Code.cpp
    This file contains the function and imports you need to send data to the ThingsBoard platform.

---
+++ Code.cpp
@@ -0,0 +1,69 @@
+// ThingsBoard IoT platform related imports and variables
+#include "http_client.h"
+String server = "https://demo.thingsboard.io"; //Your server url (a demo account can be created in https://demo.thingsboard.io)
+String access_token = "**********"; //Your access token
+
+void ThingsBoard_Send(String server, String access_token, String key, String value)
+{
+/*
+This function allow to send data to ThingsBoard servers
+server: http domine or IP f the server e.g. : "https://demo.thingsboard.io"
+access_token: is the token string for the device
+key: is the name of the variable e.g "teperature", "humedity", "presion"
+velue: is the numerical value for the variable we are mesuring
+*/
+
+// This a valid SSL certificate to be able to make HTTPS request 
+const char SSL_CA_PEM[] = "-----BEGIN CERTIFICATE-----\n"
+                          "MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n"
+                          "MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n"
+                          "DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n"
+                          "SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n"
+                          "GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n"
+                          "AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n"
+                          "q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n"
+                          "SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n"
+                          "Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n"
+                          "a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n"
+                          "/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n"
+                          "AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n"
+                          "CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n"
+                          "bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n"
+                          "c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n"
+                          "VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n"
+                          "ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n"
+                          "MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n"
+                          "Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n"
+                          "AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n"
+                          "uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n"
+                          "wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n"
+                          "X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n"
+                          "PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n"
+                          "KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n"
+                          "-----END CERTIFICATE-----\n";
+
+  
+  // Example line: "https://demo.thingsboard.io/api/v1/5bzRIpc2sta2p1agEjl2/telemetry"
+  String connection_str = server + "/api/v1/" + access_token + "/telemetry";
+
+  // Example line: "{\"temperature\": 7}"
+  String json_str = "{\"" + key + "\": " + value + "}";
+    
+  HTTPClient *httpClient = new HTTPClient(SSL_CA_PEM, HTTP_POST, connection_str.c_str());
+  
+  httpClient->set_header("Content-Type", "application/json");
+  const Http_Response* result = httpClient->send(json_str.c_str(), json_str.length());
+
+  if (result == NULL)
+  {    
+    Serial.print("Error Code: ");
+    Serial.println(httpClient->get_error());
+  }
+  else
+  {    
+    Serial.println("Body");
+    Serial.println(result->body);
+  }
+
+  delete httpClient;
+}
